1af4a7bbbfa9c71de7d8cdcd995e666568ebfc13
daniloorozco <daniloorozco@cmail.carleton.ca>
2021-04-16 17:58:50 -0400

    Init Login page w/ Login form component

--- packages/client/pages/login/login.tsx
+++ packages/client/pages/login/login.tsx
@@ -8,16 +8,16 @@ import {
     Input,
     Link,
     Spinner,
+    Text,
   } from '@chakra-ui/react';
   import React, { useState } from 'react';
-  
+  import { LoginForm } from '../../components/LoginForm';
+
   const LoginPage = () => {
   
     return (
       <>
-        <Flex>
-              
-        </Flex>
+        <LoginForm/>
       </>
     );
   };
