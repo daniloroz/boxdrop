ef1359fee499a44955982594b995e3c1baa50290
daniloorozco <daniloorozco@cmail.carleton.ca>
2021-05-05 12:41:52 -0400

    Update Sign Up form UI

--- packages/client/components/SignUpForm.tsx
+++ packages/client/components/SignUpForm.tsx
@@ -5,13 +5,15 @@ import {
     FormControl,
     FormLabel,
     Input,
+    Heading,
+    Stack,
     InputGroup,
     InputRightElement,
     Spinner,
+    Link,
   } from '@chakra-ui/react';
   import React, { useEffect, useState } from 'react';
   
-  import Link from 'next/link';
   import Router from 'next/router';
   import { useSelector } from 'react-redux';
   
@@ -22,65 +24,65 @@ import {
     const [password, setPassword] = useState<string>('');
   
     return (
-      <Flex height='100%' justifyContent='center' alignItems='center'>
-        <Box
-          as='form'
-          w={['80%', '60%']}
-          
-        >
+      //Login Area
+    <Flex minHeight='100vh' width='full' align='center' justifyContent='center'>
+    <Box 
+      borderWidth={1}
+      px={1}
+      width='full'
+      maxWidth='500px'
+      borderRadius={4}
+      textAlign='center'
+      boxShadow='lg'
+    >
+      {/*Login Header */}
+      <Box p={4} pt={10} pl={10} pr={10}>
+        <Box textAlign='left'>
+          <Heading as="h3" size="lg">Create an account</Heading>
+        </Box>
+        {/*Login Form*/}
+        <Box my={8} textAlign='left'>
+          <form>
+
+            <FormControl>
+              <FormLabel>Email</FormLabel>
+              <Input type='email' placeholder='Enter your email address' />
+            </FormControl>
+
           <FormControl>
-            <FormLabel htmlFor='email'>Email Address</FormLabel>
-            <Input
-              id='email'
-              type='email'
-              autoComplete='off'
-              required
-              color='black'
-              bg='white'
-              boxShadow='0 0 0 1px #63b3ed'
-              onChange={({ target }) => setEmail(target.value)}
-            />
-          </FormControl>
-  
-          <FormControl>
-            <FormLabel htmlFor='password'>Password</FormLabel>
+            <FormLabel mt={1} htmlFor='password'>Password</FormLabel>
             <InputGroup>
-              <Input
-                id='password'
-                type={showPassword ? 'text' : 'password'}
-                autoComplete='off'
-                required
-                mb={3}
-                color='black'
-                bg='white'
-                boxShadow='0 0 0 1px #63b3ed'
-                onChange={({ target }) => setPassword(target.value)}
-              />
-              <InputRightElement mr={1} ml={1}>
-                <Button
-                  type='submit'
-                  size='sm'
-                  onClick={() => setShowPassword(!showPassword)}
-                >
-                  {showPassword ? 'Hide' : 'Show'}
-                </Button>
-              </InputRightElement>
+                <Input
+                    id='password'
+                    placeholder='Enter your Password'
+                    type={showPassword ? 'text' : 'password'}
+                    autoComplete='off'
+                    required
+                    mb={3}
+                    boxShadow='0 0 0 1px #63b3ed'
+                    onChange={({ target }) => setPassword(target.value)}      
+                    />
+                <InputRightElement mr={1} ml={1}>
+                    <Button
+                        size='sm'
+                        onClick={() => setShowPassword(!showPassword)}
+                        >
+                                    {showPassword ? 'Hide' : 'Show'}
+                    </Button>
+                </InputRightElement>
             </InputGroup>
           </FormControl>
-  
-          <Flex flexDir='column'>
-            <Link href='/'>Already have an account?</Link>
-            <Link href='/forgot'>Forgot Your Password?</Link>
-          </Flex>
-  
-          <Button
-            type='submit'
-            w='100%'
-            mt={3}
-            bgGradient='var(--theme-color)'
-          >
-          </Button>
+
+            
+
+            <Button width='full' mt={4} colorScheme="orange">Sign In</Button>
+            <Flex mt={2} flexDir='column'>
+                <Link href='/' color='orange.500' fontSize="12">Already have an account?</Link>
+            </Flex>
+          </form>
         </Box>
-      </Flex>
+      </Box>
+    </Box>
+  </Flex>
     );
   };
