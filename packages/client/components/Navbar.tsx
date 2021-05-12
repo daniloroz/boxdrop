d3219c21045551bc91150bcab4ab2fc9e4e773bc
daniloorozco <daniloorozco@cmail.carleton.ca>
2021-05-12 16:16:16 -0400

    Set Up Navbar Skeleton

--- packages/client/components/Navbar.tsx
+++ packages/client/components/Navbar.tsx
@@ -1,50 +1,101 @@
-import { Box, Flex } from '@chakra-ui/layout';
-import { Button, Fade } from '@chakra-ui/react';
+import { ReactNode } from 'react';
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+} from '@chakra-ui/react';
+import HamburgerIcon from '@chakra-ui/icon';
+import CloseIcon from '@chakra-ui/icon'
 
-import Link from 'next/link';
-import React from 'react';
+const Links = ['Dashboard', 'Projects', 'Team'];
 
-const repoURL = 'https://github.com/daniloorozco/boxdrop';
+const NavLink = ({ children }: { children: ReactNode }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={'md'}
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('gray.200', 'gray.700'),
+    }}
+    href={'#'}>
+    {children}
+  </Link>
+);
 
 export const NavBar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <>
-      <Box className='navbar'>
-        <Flex justifyContent='space-between'>
-          <Flex
-            ml={1}
-            flexDir='column'
-            height='100%'
-            alignItems='center'
-            justifyContent='center'
-          >
-            <Link href={repoURL}>
-              <Box mt={1} cursor='pointer'>
-                <GithubSVG />
-              </Box>
-            </Link>
-          </Flex>
-          <Flex
-            justifyContent='flex-end'
-            alignItems='center'
-            height='100%'
-            mr={['3', '6']}
-          >
-            
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={5} w='100%'>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <IconButton
+            size={'md'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            display={{ md: !isOpen ? 'none' : 'inherit' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={'center'}>
+            <Box>Logo</Box>
+            <HStack
+              as={'nav'}
+              spacing={4}
+              display={{ base: 'none', md: 'flex' }}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
+          <Flex alignItems={'center'}>
+            <Menu>
+              <MenuButton
+                as={Button}
+                rounded={'full'}
+                variant={'link'}
+                cursor={'pointer'}>
+                <Avatar
+                  size={'sm'}
+                  src={
+                    'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+                  }
+                />
+              </MenuButton>
+              <MenuList>
+                <MenuItem>Link 1</MenuItem>
+                <MenuItem>Link 2</MenuItem>
+                <MenuDivider />
+                <MenuItem>Link 3</MenuItem>
+              </MenuList>
+            </Menu>
           </Flex>
         </Flex>
+
+        {isOpen ? (
+          <Box pb={4}>
+            <Stack as={'nav'} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
       </Box>
+
+      <Box p={4}>Main Content Here</Box>
     </>
   );
-};
-
-const GithubSVG = () => (
-  <svg
-    xmlns='http://www.w3.org/2000/svg'
-    width='50'
-    height='50'
-    viewBox='0 0 24 24'
-  >
-    <path d='M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z' />
-  </svg>
-);
+}
--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -3,19 +3,24 @@ import styles from '../styles/Home.module.css'
 import LoginPage from "../pages/login/login"
 import SignUpPage from "../pages/signup/signup"
 import {NavBar} from "../components/Navbar"
+import { Container, Flex } from '@chakra-ui/react';
+
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <NavBar />
       <Head>
         <title>Box Drop</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       {/*<LoginPage></LoginPage>*/}
-      <SignUpPage/>
+      {/*<SignUpPage/>*/}
       
-
+      
+      
     </div>
   )
 }
