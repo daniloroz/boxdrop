4d7b93cebdca65dc3d9f76efa2f3fc5c6690f7e5
qjimmy <quang-jimmy@hotmail.com>
2021-05-12 16:30:11 -0400

    🍆

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,26 +1,23 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import LoginPage from "../pages/login/login"
-import SignUpPage from "../pages/signup/signup"
-import {NavBar} from "../components/Navbar"
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import LoginPage from '../pages/login/login';
+import SignUpPage from '../pages/signup/signup';
+import { NavBar } from '../components/Navbar';
 import { Container, Flex } from '@chakra-ui/react';
-
-
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <NavBar />
-      <Head>
-        <title>Box Drop</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <div className={styles.container}>
+        <Head>
+          <title>Box Drop</title>
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
 
-      {/*<LoginPage></LoginPage>*/}
-      {/*<SignUpPage/>*/}
-      
-      
-      
-    </div>
-  )
+        {/*<LoginPage></LoginPage>*/}
+        {/*<SignUpPage/>*/}
+      </div>
+    </>
+  );
 }
